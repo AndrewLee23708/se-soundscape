d6879d0ce8b20887cd27c4401a5a56c78e152579
Andrew Lee <andrewlee23708@gmail.com>
2024-04-01 02:02:05 +0800

    Updated models, laid out potential endpoints, structured spotify and google api

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,69 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for, redirect, session
 from decorators import time_check
-
-#creates connection database
+import service
 from database import setup_db   #function for DB connections
 
 app = Flask(__name__)
 
+#once we have access to spotify ID and Access token, we can route to features of app
+#Authenticates the login
+@app.route('/loginAuth', methods=['GET', 'POST'])          #i dont think the users see this, so you have an authPage endpoint so we can check for login detaisl
+def loginAuth():
 
-@app.route('/')    #default path (route)
-def hello_world():
-    return 'hello world!'
+    #redirects them to spotify for login
+	token = get_token() #grab access token (for us developers)
+    
+    #We check for the following:
+    #if never logged in before, add into DB
 
 
-### DB test endpoint
-@app.route('/db-test')  # Test database connection
-def test_db():
-    try:
-        connection = setup_db()
-        cur = connection.cursor()  # All SQL is done through cursor
+    #if logged in before, pull up preexisting settings
 
-        # See how many users there are
-        cur.execute("SELECT COUNT(*) FROM Users")
-        result = cur.fetchone()  # This will be a tuple like (count,)
-        cur.close()
+	return token
 
-        # Good practice to return JSON
-        return jsonify({'number_of_users': result[0]})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#users would have access to all profiles and shared profiles
+@app.route('/profiles/<profileId>', methods=['PUT'])
+def change_profile(profileId):
+    data = request.get_json()
+    return change_profile_service(profileId, data)
+
+
+@app.route('/profiles/<profileId>', methods=['POST'])
+def add_profile(profileId):
+    data = request.get_json()
+    return add_profile_service(profileId, data)
+
+
+@app.route('/profiles/<profileId>', methods=['DELETE'])
+def delete_profile(profileId):
+    data = request.get_json()
+    return add_profile_service(profileId, data)
+
+
+@app.route('/profiles/<profileId>', methods=['POST'])
+def share_profile(profileId):
+    data = request.get_json()
+    return add_profile_service(profileId, data)
+
+
+@app.route('/pins', methods=['POST'])
+def add_pin():
+    data = request.get_json()
+    return add_pin_service(data)
+
+@app.route('/pins/<pinId>', methods=['DELETE'])
+def delete_pin(pinId):
+    return delete_pin_service(pinId)
+
+@app.route('/pins/<pinId>', methods=['PUT'])
+def edit_pin(pinId):
+    data = request.get_json()
+    return edit_pin_service(pinId, data)
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -37,6 +73,26 @@ if __name__ == '__main__':
 
 
 
+
+
+
+
+# ### DB test endpoint
+# @app.route('/db-test')  # Test database connection
+# def test_db():
+#     try:
+#         connection = setup_db()
+#         cur = connection.cursor()  # All SQL is done through cursor
+
+#         # See how many users there are
+#         cur.execute("SELECT COUNT(*) FROM Users")
+#         result = cur.fetchone()  # This will be a tuple like (count,)
+#         cur.close()
+
+#         # Good practice to return JSON
+#         return jsonify({'number_of_users': result[0]})
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
 
 
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,17 @@
-from database import get_db_connection
+from database import setup_db
 
-#simple get all users
+# models will contain 
+# it will define all queries needed for this project
+
+#LIST OF ALL QUERY METHODS:
+
+
+
+### User queries
+
+#simple get list of all users
 def get_all_users():
-    connection = get_db_connection()
+    connection = setup_db()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM users")
     users = cursor.fetchall()
@@ -10,14 +19,167 @@ def get_all_users():
     connection.close()
     return users
 
+#add new user
 def add_new_user(SpotifyUserID, Access_Token):
-    connection = get_db_connection()
+    connection = setup_db()
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO users (SpotifyUserID, Access Token) VALUES (%s, %s)", (SpotifyUserID, Access_Token))
+    cursor.execute("INSERT INTO users (SpotifyUserID, Access Token) VALUES (%s, %s)", (SpotifyUserID, Access_Token))    #parameterized queries to prevent injections?
     connection.commit()
-    user_id = cursor.lastrowid
+    user_id = cursor.lastrowid    #most recent
     cursor.close()
     connection.close()
     return user_id
 
+#delete user
+def delete_user(spotify_user_id):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Users WHERE SpotifyUserID = %s", (spotify_user_id,))
+    connection.commit()
+    deleted_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return deleted_rows     #return number of deleted rows
 
+#update user
+def update_user_access_token(spotify_user_id, access_token):
+    """
+    Updates an existing user's access token and returns the number of affected rows.
+    """
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE Users 
+        SET Access_Token = %s 
+        WHERE SpotifyUserID = %s
+        """, (access_token, spotify_user_id))
+    connection.commit()
+    updated_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return updated_rows
+
+
+### Scape Related Queries
+
+def get_all_scapes_for_user(user_id):
+    connection = setup_db()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Scape WHERE UserID = %s", (user_id,))
+    scapes = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return scapes
+
+def add_new_scape(user_id, scape_name, visibility_status, shareable_link):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO Scape (UserID, ScapeName, VisibilityStatus, ShareableLink, DateCreated) 
+        VALUES (%s, %s, %s, %s, NOW())
+        """, (user_id, scape_name, visibility_status, shareable_link))
+    connection.commit()
+    scape_id = cursor.lastrowid
+    cursor.close()
+    connection.close()
+    return scape_id
+
+# Updates an existing scape, share through links first 
+def update_scape(scape_id, scape_name, visibility_status, shareable_link):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE Scape 
+        SET ScapeName = %s, VisibilityStatus = %s, ShareableLink = %s 
+        WHERE Scape_ID = %s
+        """, (scape_name, visibility_status, shareable_link, scape_id))
+    connection.commit()
+    updated_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return updated_rows        #returns the number of affected rows.
+ 
+#delete a Scape
+def delete_scape(scape_id):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Scape WHERE Scape_ID = %s", (scape_id,))
+    connection.commit()
+    deleted_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return deleted_rows         #returns the number of affected rows.
+
+
+
+### Pin related queries
+
+# Load all pins for a specific profile (scape), allows us to load all pins available on map 
+# Returns a table of pins and their info
+# Decision to load only initial pin attributes initially, may change later on
+def get_pins_by_profile(scape_id):
+    connection = setup_db()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("""
+        SELECT Pin_ID, Latitude, Longitude 
+        FROM Pin 
+        INNER JOIN Scape ON Pin.ScapeID = Scape.Scape_ID 
+        WHERE Scape.Scape_ID = %s""", (scape_id,))
+    pins = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return pins     
+
+# Load pin detail (when user selects a pin)
+# when users select pin, this shows up
+def get_pin_details(pin_id):
+    connection = setup_db()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("""
+        SELECT * 
+        FROM Pin 
+        WHERE Pin_ID = %s
+        """, (pin_id,))
+    pin_details = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return pin_details
+
+#Add pins
+def pin_add(ScapeID, Location, Latitude, Longitude, Radius, Priority, PlaylistID, DateCreated):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute(""" INSERT INTO Pin (ScapeID, Location, Latitude, Longitude, Radius, Priority, PlaylistID, DateCreated)         
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", 
+        (ScapeID, Location, Latitude, Longitude, Radius, Priority, PlaylistID, DateCreated))                   #note, careful not to change pin_id (AUTO INCREMENT)
+    connection.commit()
+    pin_id = cursor.lastrowid  # Corrected variable name to reflect that it's the ID of the pin
+    cursor.close()
+    connection.close()
+    return pin_id  # Return the ID of the newly added pin
+
+# Update an existing pin
+def update_pin(Pin_ID, ScapeID, Location, Latitude, Longitude, Radius, Priority, PlaylistID):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE Pin 
+        SET ScapeID = %s, Location = %s, Latitude = %s, Longitude = %s, Radius = %s, Priority = %s, PlaylistID = %s 
+        WHERE Pin_ID = %s """, 
+        (ScapeID, Location, Latitude, Longitude, Radius, Priority, PlaylistID, Pin_ID))
+    connection.commit()
+    updated_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return updated_rows
+
+# Delete a pin
+def delete_pin(Pin_ID):
+    connection = setup_db()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Pin WHERE Pin_ID = %s", (Pin_ID,))
+    connection.commit()
+    deleted_rows = cursor.rowcount
+    cursor.close()
+    connection.close()
+    return deleted_rows
